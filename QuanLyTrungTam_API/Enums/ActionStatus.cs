3e0541e79d6e81273c577073b395c3bfd681c81e
pvq2k2 <vanquyet2392002@gmail.com>
2023-08-17 15:36:41 +0700

    Create Response # Enums Action Status

---
+++ QuanLyTrungTam_API/Enums/ActionStatus.cs
@@ -0,0 +1,9 @@
+﻿namespace QuanLyTrungTam_API.Enums
+{
+    public enum ActionStatus
+    {
+        Success = 200,
+        BadRequest = 400,
+        NotFound = 404
+    }
+}
---
+++ QuanLyTrungTam_API/Handle/Response/ResponseData.cs
@@ -0,0 +1,12 @@
+﻿using QuanLyTrungTam_API.Enums;
+
+namespace QuanLyTrungTam_API.Handle.Response
+{
+    public class ResponseData<T>
+    {
+        public ActionStatus Status { get; set; }
+        public string Message { get; set; } = string.Empty;
+        public T? Data { get; set; }
+        public List<T>? DataList { get; set; }
+    }
+}
